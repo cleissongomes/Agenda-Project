261a410f7ac63417209c495fd7e0391210b90022
cleissongomes <cleissongomes777@gmail.com>
2023-06-20 14:55:10 -0300

    first commit

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,19 @@
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.aLocalVariable = 'This is the value of the local variable.';
+    next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+    next();
+};
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if(err && 'EBADCSRFTOKEN' === err.code) {
+        return res.render('404');
+    }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+};
