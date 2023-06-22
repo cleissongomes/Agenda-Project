149a14999e3eb55bdc31f265284199a30fc1d44d
cleissongomes <cleissongomes777@gmail.com>
2023-06-22 11:42:44 -0300

    feat: Import loginController and add GET route for login

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
 route.get('/', homeController.index);
+
+route.get('/login/index', loginController.index);
 
 
 
