8879747c0e82f19c0f6730e08cb078aa233af996
cleissongomes <cleissongomes777@gmail.com>
2023-06-26 11:21:14 -0300

    refactor: Update LoginModel variable and start building Login class in LoginModel.js

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 const LoginSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: String 
+    email: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
+    constructor(body) {
+        this.body = body;
+        this.errors = [];
+        this.user = null;
+    }
 
 }
 
