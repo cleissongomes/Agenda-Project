b4f0c5f2ad3a42f5b1eb486e11ac27c268b0bfcd
cleissongomes <cleissongomes777@gmail.com>
2023-06-28 13:52:12 -0300

    feat:Create method valida in LoginModel.js

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { register } = require('../controllers/loginController');
+const validator = require('validator');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -15,9 +15,17 @@ class Login {
         this.user = null;
     }
 
+    valida() {
+        this.cleanUp();
+        if(!validator.isEmail(this.body.email)) this.error.push('Invalid email address.');
+        if(this.body.password.length < 3 || this.body.password.length > 50 ) {
+            this.errors.push('Invalid Password: Password must be between 3 and 50 characters long.')
+        }
+    }
+
     cleanUp() {
         for(const key in this.body) {
-            if(!typeof this.body[key] !== 'string') {
+            if(typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
