8935a582b74bb00619debbe5b649ef65715163df
cleissongomes <cleissongomes777@gmail.com>
2023-06-28 14:08:29 -0300

    fix: Correct spelling of 'errors' in validate method and update method name to 'validate'

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -22,7 +22,7 @@ class Login {
 
     valida() {
         this.cleanUp();
-        if(!validator.isEmail(this.body.email)) this.error.push('Invalid email address.');
+        if(!validator.isEmail(this.body.email)) this.errors.push('Invalid email address.');
         if(this.body.password.length < 3 || this.body.password.length > 50 ) {
             this.errors.push('Invalid Password: Password must be between 3 and 50 characters long.')
         }
