6db6ef21ae503eb1653b5849d5762c9fac40c2db
cleissongomes <cleissongomes777@gmail.com>
2023-06-30 20:29:00 -0300

    refactor: Edit register method in loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,11 +1,21 @@
+const { async } = require('regenerator-runtime');
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
     res.render('login');
 };
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
     const login = new Login(req.body);
-    login.register();
+    await login.register();
+
+    if(login.errors.length > 0) {
+        req.flash('errors', login.errors);
+        req.session.save(() => {
+            return req.redirect('back');
+        });
+        return;
+    }
+
     res.send(login.errors);
 };
