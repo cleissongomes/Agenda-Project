1b1a68a73ebba9a725e7f1fc7215dabbf5428bf9
cleissongomes <cleissongomes777@gmail.com>
2023-07-04 10:57:17 -0300

    chore: Update server port in server.js

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('ready', () => {  
-app.listen(5000, () => {
-    console.log('Acessar http://localhost:5000');
-    console.log('Servidor executando na porta 5000');
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
 });
 });
