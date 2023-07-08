e955521911ed01aef245265f0e602817061eff8f
cleissongomes <cleissongomes777@gmail.com>
2023-07-07 22:55:26 -0300

    feat: Create route and method login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,4 @@
+const { async } = require('regenerator-runtime');
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
@@ -27,4 +28,8 @@ exports.register = async function(req, res) {
         console.log(e);
         return res.render('404');
     }
+};
+
+exports.login = async function(req, res) {
+
 };
