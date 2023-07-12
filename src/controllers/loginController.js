b58799670f2ed4e1cffc11975699ee407e30673a
cleissongomes <cleissongomes777@gmail.com>
2023-07-11 23:51:42 -0300

    feat:Update index function in loginController.js  and create login-logado in views.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,7 +2,8 @@ const { async } = require('regenerator-runtime');
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-   res.render('login');
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 exports.register = async function(req, res) {
