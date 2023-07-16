397a2bc30f6df72e44f57f852774fb87250813ea
cleissongomes <cleissongomes777@gmail.com>
2023-07-16 15:19:21 -0300

    chore: Update contactSchema in contactModel,js

--- src/models/ContactModel.js
+++ src/models/ContactModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const ContactSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    surname: { type: String, required: true },
-    email: { type: String, required: true },
-    Telephone: { type: String, required: true }
+    surname: { type: String, required: false, default: '' },
+    email: { type: String, required: false, default: '' },
+    telephone: { type: String, required: false, default: '' },
+    date: { type: Date, default: Date.now },
 });
 
 
