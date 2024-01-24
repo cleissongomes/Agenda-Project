fb27ee2a5a73fe6ac9a8bc99168c9d11c09e3e55
cleissongomes <cleissongomes777@gmail.com>
2024-01-23 22:39:01 -0300

    feat(routes, controllers, models): add new route in routes.js, implement 'edit' function in contactControllers.js, create new function in models.js

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -2,38 +2,63 @@ const { async } = require('regenerator-runtime');
 const Contact = require('../models/ContactModel');
 
 exports.index = (req, res) => {
-    res.render('contact', {
-        contact: {}
-    });
+  res.render('contact', {
+    contact: {},
+  });
 };
 
 exports.register = async (req, res) => {
-    try {
-        const contact = new Contact(req.body);
-        await contact.register();
+  try {
+    const contact = new Contact(req.body);
+    await contact.register();
 
-        if(contact.errors.length > 0) {
-            req.flash('errors', contact.errors);
-            req.session.save(() => res.redirect('/contact/index'));
-            return;
-        }
-    
-        req.flash('success', 'Contact successfully registered.');
-        req.session.save(() => res.redirect(`/contact/index${contact.contact._id}`));
-        return;
-    } catch(e) {
-        console.log(e);
-        return res.render('');
+    if (contact.errors.length > 0) {
+      req.flash('errors', contact.errors);
+      req.session.save(() => res.redirect('/contact/index'));
+      return;
     }
+
+    req.flash('success', 'Contact successfully registered.');
+    req.session.save(() =>
+      res.redirect(`/contact/index${contact.contact._id}`)
+    );
+    return;
+  } catch (e) {
+    console.log(e);
+    return res.render('');
+  }
 };
 
 exports.editIndex = async (req, res) => {
-    if(!req.params.id) return res.render('404');
+  if (!req.params.id) return res.render('404');
 
-    const contact = new Contact();
-    await contact.searchById(req.params.id);
+  const contact = new Contact();
+  await contact.searchById(req.params.id);
 
-    if(!contact) return res.render('404');
+  if (!contact) return res.render('404');
 
-    res.render('contact', { contact });
+  res.render('contact', { contact });
+};
+
+exports.edit = async function (req, res) {
+  try {
+    if (!req.params.id) return res.render('404');
+    const contact = new Contact(req.body);
+    await contact.edit(req.params.id);
+
+    if (contact.errors.length > 0) {
+      req.flash('errors', contact.errors);
+      req.session.save(() => res.redirect('/contact/index'));
+      return;
+    }
+
+    req.flash('success', 'Contact successfully edited.');
+    req.session.save(() =>
+      res.redirect(`/contact/index${contact.contact._id}`)
+    );
+    return;
+  } catch (e) {
+    console.log(e);
+    res.render('404');
+  }
 };
--- src/models/ContactModel.js
+++ src/models/ContactModel.js
@@ -3,58 +3,69 @@ const { async } = require('regenerator-runtime');
 const validator = require('validator');
 
 const ContactSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    surname: { type: String, required: false, default: '' },
-    email: { type: String, required: false, default: '' },
-    telephone: { type: String, required: false, default: '' },
-    date: { type: Date, default: Date.now },
+  name: { type: String, required: true },
+  surname: { type: String, required: false, default: '' },
+  email: { type: String, required: false, default: '' },
+  telephone: { type: String, required: false, default: '' },
+  date: { type: Date, default: Date.now },
 });
-
 
 const ContactModel = mongoose.model('Contact', ContactSchema);
 
 class Contact {
-    constructor(body) {
-        this.body = body;
-        this.errors = [];
-        this.contact = null;
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.contact = null;
+  }
+
+  async searchById(id) {
+    if (typeof id !== 'string') return;
+    const user = await ContactModel.findById(id);
+    return user;
+  }
+
+  async register() {
+    this.validate();
+    if (this.errors.length > 0) return;
+    this.contact = await ContactModel.create(this.body);
+  }
+
+  validate() {
+    this.cleanUp();
+    if (this.body.email && !validator.isEmail(this.body.email))
+      this.errors.push('Invalid email address.');
+    if (!this.body.name) this.errors.push('Name is a required field.');
+    if (!this.body.email && !this.body.telephone) {
+      this.errors.push(
+        'At least one contact needs to be sent: email or phone.'
+      );
+    }
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
     }
 
-    async searchById(id) {
-        if(typeof id !== 'string') return;
-        const user = await ContactModel.findById(id);
-        return user;
-    } 
-
-    async register(){
-        this.validate();
-        if(this.errors.length > 0) return;
-        this.contact = await ContactModel.create(this.body);
-    }
-
-    validate() {
-        this.cleanUp();
-        if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('Invalid email address.');
-        if(!this.body.name) this.errors.push('Name is a required field.');
-        if(!this.body.email && !this.body.telephone) { 
-            this.errors.push('At least one contact needs to be sent: email or phone.');
-        }
-    }
-
-    cleanUp() {
-        for(const key in this.body) {
-            if(typeof this.body[key] !== 'string') {
-                this.body[key] = '';
-            }
-        }
-
-        this.body = {
-            name: this.body.name,
-            surname: this.body.surname,
-            email: this.body.email,
-            telephone: this.body.telephone 
-        };
-    }
+    this.body = {
+      name: this.body.name,
+      surname: this.body.surname,
+      email: this.body.email,
+      telephone: this.body.telephone,
+    };
+  }
 }
+
+Contact.prototype.edit = async function (id) {
+  if (typeof id !== 'string') return;
+  this.validate();
+  if (this.error.length > 0) return;
+  this.contact = await ContactModel.findByIdAndUpdate(id, this.body, {
+    new: true,
+  });
+};
 
 module.exports = Contact;
